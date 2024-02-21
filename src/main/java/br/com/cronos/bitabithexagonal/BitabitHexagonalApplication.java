f575284594428a10d61159b4f119edc4cb3ba5c3
rodrigo <rodrigo.souza@goharry.com.br>
2024-02-21 13:54:58 -0300

    [FEATURE][ADD] - criando o projeto

---
+++ src/main/java/br/com/cronos/bitabithexagonal/BitabitHexagonalApplication.java
@@ -0,0 +1,13 @@
+package br.com.cronos.bitabithexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitabitHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitabitHexagonalApplication.class, args);
+    }
+
+}
